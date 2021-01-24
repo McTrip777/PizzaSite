ddce7d2fab967b9457a218d6227cd0fa93dc6258
Jacob <jacob.m.mcfaul@gmail.com>
2021-01-23 18:17:00 -0700

    Home page is done aside from a couple styling changes and adding the footer

--- pizza/src/components/Home.jsx
+++ pizza/src/components/Home.jsx
@@ -25,10 +25,20 @@ function Home() {
                 <h3 className="contactTitle">Contact</h3>
                 <div className="innerContactContainer">
                     <MapComponent /> {/* className = mapContainer */}
-                    <div className="contactInfo">
+                    <div
+                        className="contactInfo">
+                        <h5 className="numberHeader">Call us at:</h5>
                         <p className="contactNumber">(208) 888-4857</p>
-                        <h5 className="addressHeader">Address</h5>
-                        <p className="address">1526 N Main St, Meridian, ID 83642</p>
+                        <h5 className="addressHeader">Where to find us!</h5>
+                        <a target="_blank"
+                            href="https://www.google.com/maps/dir//Johnny's+Pizza,+1526+N+Main+St,+Meridian,+ID+83642/@43.6177695,-116.3911977,19z/data=!4m8!4m7!1m0!1m5!1m1!1s0x54ae538d04a0c13f:0x40f2531f811a14b!2m2!1d-116.3911832!2d43.61764">
+                            <p className="address">1526 N Main St, Meridian, ID 83642</p>
+                        </a>
+                        <div className="socials">
+                            <Link to="/facebook"><i className="fab fa-facebook fa-2x"></i></Link>
+                            <Link to="/instagram"><i className="fab fa-instagram fa-2x"></i></Link>
+                            <Link to="/twitter"><i className="fab fa-twitter fa-2x"></i></Link>
+                        </div>
                     </div>
                 </div>
             </div>
