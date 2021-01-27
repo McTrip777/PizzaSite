c728e98890cfb4758b6b28e629e42b39f406602a
Jacob <jacob.m.mcfaul@gmail.com>
2021-01-27 13:45:14 -0700

    contact media sizing fixed

--- pizza/src/components/Footer.jsx
+++ pizza/src/components/Footer.jsx
@@ -4,21 +4,27 @@ import { HashLink } from 'react-router-hash-link'
 
 function Footer() {
     return (
-        <div className="footer">
-            <div className="foo footerLinks">
-                <HashLink smooth to="/#home">Home</HashLink>
-                <Link to="/menu">Menu</Link>
-                <HashLink smooth to="/#contact">Contact</HashLink>
-            </div>
-            <div className="foo footerContact">
-                <h4>Contact Us</h4>
-                <p className="number">(208) 888-4857</p>
-                <p className="email">contact@JohnnysPizza.com</p>
-                <div className="socials">
-                    <Link to="/facebook"><i className="fab fa-facebook fa-2x"></i></Link>
-                    <Link to="/instagram"><i className="fab fa-instagram fa-2x"></i></Link>
-                    <Link to="/twitter"><i className="fab fa-twitter fa-2x"></i></Link>
+        <div className="footerContainer">
+            <div className="footer">
+                <div className="foo footerLinks">
+                    <HashLink smooth to="/#home">Home</HashLink>
+                    <Link to="/menu">Menu</Link>
+                    <HashLink smooth to="/#contact">Contact</HashLink>
                 </div>
+                <div className="foo footerContact">
+                    <h4>Contact Us</h4>
+                    <p className="number">(208) 888-4857</p>
+                    <p className="email">contact@JohnnysPizza.com</p>
+                    <div className="socials">
+                        <Link to="/facebook"><i className="fab fa-facebook fa-2x"></i></Link>
+                        <Link to="/instagram"><i className="fab fa-instagram fa-2x"></i></Link>
+                        <Link to="/twitter"><i className="fab fa-twitter fa-2x"></i></Link>
+                    </div>
+                </div>
+            </div>
+            <div className="createdBy">
+                <p>Copyright &copy; 2021 All Rights Reserved</p>
+                <p>Created By Jacob McFaul</p>
             </div>
         </div>
     )
